9b3112a288d2a42478bf34c9a17090a6298c1707
MinorJerry <3160103176@zju.edu.cn>
2018-11-24 15:56:54 +0800

    v2

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #define MAXNUM 100
 typedef enum
 {
@@ -16,61 +18,36 @@ struct car_info
 };
 struct queue
 {
-    int num;//the number of the car in this direction
-    direction dir; //the direction
-    int carID[MAXNUM];//the ID of the car in this direction
-    int front;//the front pointer of the queue
-    int rear;//the rear pointer of the queue
+    int num;           //the number of the car in this direction
+    direction dir;     //the direction
+    int carID[MAXNUM]; //the ID of the car in this direction
+    int front;         //the front pointer of the queue
+    int rear;          //the rear pointer of the queue
 };
-pthread_mutex_t mutex_source[4];//represent the a b c d source，the order is c b a d
+pthread_mutex_t mutex_source[4]; //represent the a b c d source，the order is c b a d
 
 pthread_mutex_t mutex_dir[4];
-pthread_mutex_t mutex_waitQueue[4];
-pthread_cond_t cond_queue[4];
 
+pthread_cond_t first_dir[4];
 struct queue dirQueue[4];
-int first_queue[4];//the first car's ID in each direction(queue)
 
-void *car_thread_handle(void *pParam)
+int wait_num = 0;
+
+void initMut()
 {
-    struct car_info *car = (struct car_info *)pParam;
-    dirQueue[car->dir].carID[++dirQueue[car->dir].rear] = car->id;//push_back the car id into the queue
-    dirQueue[car->dir].num++;//let the num++
-    char dir_s[10];
-    //get the dir string which will be used to print the information
-    switch(car->dir)
+    for (int i = 0; i < 4; i++)
     {
-        case 0:
-            strcpy(dir_s,"North");break;
-        case 1:
-            strcpy(dir_s,"East");break;
-        case 2:
-            strcpy(dir_s,"South");break;
-        case 3:
-            strcpy(dir_s,"West");break;
+        pthread_mutex_init(&mutex_source[i], NULL);
+        pthread_mutex_init(&mutex_dir[i], NULL);
     }
-    //if the car is not the first car, it should wait
-    while(first_queue[car->dir]!=car->id);
-    //If a car is passing the cross, others should wait in this wait queue.
-    pthread_mutex_lock(&mutex_dir[car->dir]);
-    
-    printf("Car %d from %s arrives at crossing",car->id,dir_s);//print the arriving info
-    
-
-    //wait for the signal to occupy one source
-    pthread_cond_wait(&cond_queue[car->dir],&mutex_dir[car->dir]);
-
-    //unlock
-    pthread_mutex_unlock(&mutex_dir[car->dir]);
-
 }
-int main(int argc, char **argv)
+void initCond()
 {
-    char carList[100] = argv[1];
-    int carNum = strlen(carList);
-    pthread_t car_thread[carNum];
-    struct car_info car[carNum];
-    //iniitial the counter of each direction 
+    for (int i = 0; i < 4; i++)
+        pthread_cond_init(&first_dir[i], NULL);
+}
+void initQueue()
+{
     for (int i = 0; i < 4; i++)
     {
         dirQueue[i].num = 0;
@@ -78,28 +55,127 @@ int main(int argc, char **argv)
         dirQueue[i].front = 0;
         dirQueue[i].rear = -1;
     }
+}
+void *car_thread_handle(void *pParam)
+{
+
+    struct car_info *car = (struct car_info *)pParam;
+
+    dirQueue[car->dir].carID[++dirQueue[car->dir].rear] = car->id; //push_back the car id into the queue
+
+    dirQueue[car->dir].num++; //let the num++
+
+    char dir_s[10], dir_left_s[10];
+    int right_dir;
+    int wait_flag = 0;
+    //get the dir string which will be used to print the information
+    switch (car->dir)
+    {
+    case 0:
+        strcpy(dir_s, "North");
+        strcpy(dir_left_s, "East");
+        break;
+    case 1:
+        strcpy(dir_s, "East");
+        strcpy(dir_left_s, "South");
+        break;
+    case 2:
+        strcpy(dir_s, "South");
+        strcpy(dir_left_s, "West");
+        break;
+    case 3:
+        strcpy(dir_s, "West");
+        strcpy(dir_left_s, "North");
+        break;
+    }
+
+    while (dirQueue[car->dir].carID[dirQueue[car->dir].front] != car->id)
+        ;
+    //If a car is passing the cross, others should wait in this waitting queue.
+
+    pthread_mutex_lock(&mutex_dir[car->dir]);
+    usleep(1000);
+    printf("Car %d from %s arrives at crossing\n", car->id, dir_s); //print the arriving info
+
+    if (wait_num == 3) // Now The car want to occupy the source, but if there will be a deadlock, it can not occupy
+    {
+        printf("DEADLOCK: car jam detected, signaling %s to go\n", dir_left_s);
+        usleep(2000);
+        pthread_cond_signal(&first_dir[(car->dir + 1) % 4]);
+    }
+
+    while (wait_num == 3)
+        ; //let the thread block here,until the left car has occupied the source
+    wait_num++;
+    pthread_mutex_lock(&mutex_source[car->dir]);
+    usleep(1000);
+
+    right_dir = (car->dir + 3) % 4;
+
+    if (dirQueue[right_dir].num) //There is at least a car at right
+    {
+
+        pthread_cond_wait(&first_dir[car->dir], &mutex_dir[car->dir]); //wait the right to go first   Maybe BUG
+    }
+
+    pthread_mutex_lock(&mutex_source[(car->dir - 1) % 4]);
+    dirQueue[car->dir].num--;
+    usleep(1000);
+    wait_num--;
+    printf("Car %d from %s leaving crossing\n", car->id, dir_s);
+
+    pthread_mutex_unlock(&mutex_source[car->dir]);
+
+    pthread_cond_signal(&first_dir[(car->dir + 1) % 4]);
+    usleep(2000);
+
+    dirQueue[car->dir].front++;
+    pthread_mutex_unlock(&mutex_source[(car->dir - 1) % 4]);
+    usleep(1000);
+    //unlock
+    pthread_mutex_unlock(&mutex_dir[car->dir]);
+}
+int main(int argc, char **argv)
+{
+    char carList[100];
+    strcpy(carList, argv[1]);
+    int carNum = strlen(carList);
+    pthread_t car_thread[carNum];
+    struct car_info *car;
+
+    //initialize the mutex and cond
+    initMut();
+    initCond();
+    //initialize the counter of each direction
+    initQueue();
     //creat the threads
     for (int i = 0; i < carNum; i++)
     {
-        car[i].id = i;
+        car = (struct car_info *)malloc(sizeof(struct car_info));
+        //car[i].id = i;
+        car->id = i;
         switch (carList[i])
         {
         case 'n':
-            car[i].dir = north;
+            //car[i].dir = north;
+            car->dir = north;
             break;
         case 'e':
-            car[i].dir = east;
+            //car[i].dir = east;
+            car->dir = east;
             break;
         case 'w':
-            car[i].dir = west;
+            //car[i].dir = west;
+            car->dir = west;
             break;
         case 's':
-            car[i].dir = south;
+            //car[i].dir = south;
+            car->dir = south;
             break;
         default:
             break;
         }
-        pthread_create(&car_thread[i], NULL, car_thread_handle, (void *)&car[i]);
+        pthread_create(&car_thread[i], NULL, car_thread_handle, car);
     }
     //wait for the threads to quit
     for (int i = 0; i < carNum; i++)
