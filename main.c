e8072b10cba1eb7e7afdcb4645aa99788af1ceb5
ZJUHeHongLiang <3160103176@zju.edu.cn>
2018-12-26 22:58:43 +0800

    final_version

--- main.c
+++ main.c
@@ -61,9 +61,9 @@ void *car_thread_handle(void *pParam)
 
     struct car_info *car = (struct car_info *)pParam;
 
-    dirQueue[car->dir].carID[++dirQueue[car->dir].rear] = car->id; //push_back the car id into the queue
+    //dirQueue[car->dir].carID[++dirQueue[car->dir].rear] = car->id; //push_back the car id into the queue
 
-    dirQueue[car->dir].num++; //let the num++
+    //dirQueue[car->dir].num++; //let the num++
 
     char dir_s[10], dir_left_s[10];
     int right_dir;
@@ -94,13 +94,13 @@ void *car_thread_handle(void *pParam)
     //If a car is passing the cross, others should wait in this waitting queue.
 
     pthread_mutex_lock(&mutex_dir[car->dir]);
-    usleep(1000);
+    usleep(2000);
     printf("Car %d from %s arrives at crossing\n", car->id, dir_s); //print the arriving info
 
     if (wait_num == 3) // Now The car want to occupy the source, but if there will be a deadlock, it can not occupy
     {
         printf("DEADLOCK: car jam detected, signaling %s to go\n", dir_left_s);
-        usleep(2000);
+        usleep(4000);
         pthread_cond_signal(&first_dir[(car->dir + 1) % 4]);
     }
 
@@ -108,7 +108,7 @@ void *car_thread_handle(void *pParam)
         ; //let the thread block here,until the left car has occupied the source
     wait_num++;
     pthread_mutex_lock(&mutex_source[car->dir]);
-    usleep(1000);
+    usleep(2000);
 
     right_dir = (car->dir + 3) % 4;
 
@@ -120,18 +120,18 @@ void *car_thread_handle(void *pParam)
 
     pthread_mutex_lock(&mutex_source[(car->dir - 1) % 4]);
     dirQueue[car->dir].num--;
-    usleep(1000);
+    usleep(2000);
     wait_num--;
     printf("Car %d from %s leaving crossing\n", car->id, dir_s);
 
     pthread_mutex_unlock(&mutex_source[car->dir]);
 
     pthread_cond_signal(&first_dir[(car->dir + 1) % 4]);
-    usleep(2000);
+    usleep(4000);
 
     dirQueue[car->dir].front++;
     pthread_mutex_unlock(&mutex_source[(car->dir - 1) % 4]);
-    usleep(1000);
+    usleep(2000);
     //unlock
     pthread_mutex_unlock(&mutex_dir[car->dir]);
 }
@@ -141,7 +141,7 @@ int main(int argc, char **argv)
     strcpy(carList, argv[1]);
     int carNum = strlen(carList);
     pthread_t car_thread[carNum];
-    struct car_info *car;
+    struct car_info *car[carNum];
 
     //initialize the mutex and cond
     initMut();
@@ -151,31 +151,37 @@ int main(int argc, char **argv)
     //creat the threads
     for (int i = 0; i < carNum; i++)
     {
-        car = (struct car_info *)malloc(sizeof(struct car_info));
+        car[i] = (struct car_info *)malloc(sizeof(struct car_info));
         //car[i].id = i;
-        car->id = i;
+        car[i]->id = i;
         switch (carList[i])
         {
         case 'n':
             //car[i].dir = north;
-            car->dir = north;
+            car[i]->dir = north;
             break;
         case 'e':
             //car[i].dir = east;
-            car->dir = east;
+            car[i]->dir = east;
             break;
         case 'w':
             //car[i].dir = west;
-            car->dir = west;
+            car[i]->dir = west;
             break;
         case 's':
             //car[i].dir = south;
-            car->dir = south;
+            car[i]->dir = south;
             break;
         default:
             break;
         }
-        pthread_create(&car_thread[i], NULL, car_thread_handle, car);
+        //pthread_create(&car_thread[i], NULL, car_thread_handle, car);
+        dirQueue[car[i]->dir].carID[++dirQueue[car[i]->dir].rear]=i;
+        dirQueue[car[i]->dir].num++;
+    }
+    for(int i=0;i<carNum;i++)
+    {
+        pthread_create(&car_thread[i], NULL, car_thread_handle, car[i]);
     }
     //wait for the threads to quit
     for (int i = 0; i < carNum; i++)
